b9ac2fe449406bd070a32fcc7145def735016fed
Jun <nbc7799@naver.com>
2025-07-22 18:11:19 +0900

    feat/Route 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
-
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>asdasd</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
