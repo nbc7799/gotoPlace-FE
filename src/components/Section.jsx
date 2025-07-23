1054462ac3d3e57848ea86ac8e791ca48df0b65e
Jun <nbc7799@naver.com>
2025-07-23 10:08:15 +0900

    feat/section title수정

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PlaceCard from './PlaceCard';
 
-export default function Section() {
+export default function Section({ title }) {
   return (
     <section className="flex flex-col gap-5 p-8">
-      <h2>찜한 맛집</h2>
+      <h2>{title}</h2>
       <div className="flex">
         <PlaceCard />
         <PlaceCard />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import Section from '@/components/Section';
 export default function Home() {
   return (
     <div className="h-full w-full bg-blue-300">
-      <Section />
-      <Section />
+      <Section title={'찜한 맛집'} />
+      <Section title={'맛집 목록'} />
     </div>
   );
 }
