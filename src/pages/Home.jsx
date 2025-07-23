e6fea85026a2db90a076543c5785b573d448d027
Jun <nbc7799@naver.com>
2025-07-23 15:11:22 +0900

    feat/api호출성공

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
 import Section from '@/components/Section';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/places')
+      .then(response => {
+        setPlaces(response.data.places);
+        console.log(response.data.places);
+      })
+      .catch(err => {
+        console.log('에러발생!', err);
+      });
+  }, []);
+
+  if (loading) return;
   return (
     <div className="h-full w-full bg-blue-300">
       <Section title={'찜한 맛집'} />
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
