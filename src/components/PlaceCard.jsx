3858f07c27c06a0fd504bc8063c1e2b69aa900e6
Jun <nbc7799@naver.com>
2025-07-24 11:07:24 +0900

    feat/무언가 업데이트 (#7)

--- src/components/PlaceCard.jsx
+++ src/components/PlaceCard.jsx
@@ -2,13 +2,13 @@ import { BASE_URL } from '../constants/baseURL';
 
 export default function PlaceCard({ item }) {
   return (
-    <div className="aspect-[0.8] bg-stone-200">
+    <div className="aspect-[0.8] bg-stone-200 text-center">
       <img
         className="h-full w-full rounded-lg object-cover"
         src={`${BASE_URL}/${item.image.src}`}
         alt={item.image.alt}
       />
-      <h2 className="text-lg">{item.title}</h2>
+      <h2 className="text-lg text-gray-900">{item.title}</h2>
     </div>
   );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -23,7 +23,7 @@ export default function Section({ title, endpoint }) {
   if (loading) return <p>맛집 소환중!</p>;
 
   return (
-    <section className="flex flex-col gap-6 p-8">
+    <section className="flex flex-col items-center gap-6 p-8">
       <h2>{title}</h2>
       <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
         {places.map((item, index) => {
