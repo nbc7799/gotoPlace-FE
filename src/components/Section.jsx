d7a13ba63283b7c66adedeaec80545a08ea6e542
Jun <nbc7799@naver.com>
2025-07-24 15:42:39 +0900

    feat/ loading message (#8)

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,21 +6,33 @@ import { getPlace } from '../api/getPlace';
 export default function Section({ title, endpoint }) {
   const [loading, setLoading] = useState(true);
   const [places, setPlaces] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    getPlace(endpoint, setLoading, setPlaces);
+    getPlace(endpoint, setLoading, setPlaces, setError);
   }, []);
-
-  if (loading) return <p>맛집 소환중!</p>;
 
   return (
     <section className="flex flex-col items-center gap-6 p-8">
       <h2>{title}</h2>
-      <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
-        {places.map((item, index) => {
-          return <PlaceCard key={index} item={item} />;
-        })}
-      </div>
+
+      {loading ? (
+        <div className="bg-gray-100 p-8">
+          <p>맛집 소환중!</p>
+        </div>
+      ) : error ? (
+        <div className="bg-red-100 p-8">{error}</div>
+      ) : places ? (
+        <div className="grid grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
+          {places.map((item, index) => {
+            return <PlaceCard key={index} item={item} />;
+          })}
+        </div>
+      ) : (
+        <div className="bg-gray-50 p-8">
+          <p>목록이 비었습니다</p>
+        </div>
+      )}
     </section>
   );
 }
