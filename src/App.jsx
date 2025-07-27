1cc4983ac6f9ad4036fdd57b015ef35104ae6d38
Jun <nbc7799@naver.com>
2025-07-27 15:15:03 +0900

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
