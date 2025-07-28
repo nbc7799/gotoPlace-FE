5073e67e3a656366217fa55c8210fcd71f0c544f
Jun <nbc7799@naver.com>
2025-07-28 22:15:21 +0900

    feat/거리순정렬로직 구현

--- src/api/getPlace.js
+++ src/api/getPlace.js
@@ -1,20 +1,21 @@
 import axios from 'axios';
 import { BASE_URL } from '../constants/baseURL';
 
+//getPlace 비동기 함수로 endpoint를 매개변수로 받아 api 요청을 보낼 주소의 뒷부분으로 활용
 export const getPlace = async endpoint => {
   try {
+    //axios로 get요청보냄  기본 url에 뒤에 endpoint더해서 해당 데이터 return해줌
     const response = await axios.get(`${BASE_URL}/${endpoint}`);
     return response.data.places;
   } catch (err) {
+    //에러가 발생시 에러에 응답
     if (err.response) {
       const status = err.response.status;
       switch (status) {
         case 404:
           throw new Error('요청하신 리소스를 찾을 수 없습니다.(404)');
-          break;
         case 500:
           throw new Error('서버에 문제가 발생했습니다. (500)');
-          break;
         default:
           throw new Error(`에러발생! 상태코드: ${status}`);
       }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,33 +6,54 @@ import { sortPlacesByDistance } from '../utils/loc';
 
 export default function Section({ title, endpoint }) {
   const [loading, setLoading] = useState(true);
+  //실제 보여줄 장소 목록
   const [places, setPlaces] = useState([]);
+  //초기 원본 장소 목록(정렬 전 상태 저장해두기 위함)
+  const [originalPlaces, setOriginalPlaces] = useState([]);
   const [error, setError] = useState('');
   const { location, error: locError } = useUserLocation();
-  const [isDefaultSort, setIsDefaultSort] = useState(true);
+  // 현재 정렬 상태가 기본 순서인지 거리 순인지 관리
+  const [isDefaultSort, setIsDefaultSort] = useState(false);
 
+  // 컴포넌트가 처음 마운트되거나 endpoint가 바뀔 때 실행
   useEffect(() => {
+    //비동기 함수 선언(api 호출 및 데이터 세팅)
     const initPlace = async () => {
-      setLoading(true);
+      setLoading(true); // 로딩 시작
       try {
-        const data = await getPlace(endpoint);
-        setPlaces(data);
+        //getPlace내부에서 비동기네트워크요청을 하고있어서 결과를 await로 기다려야한다.
+        //그렇기에 자연스럽게 initPlace도 async함수가 되어야함
+        const data = await getPlace(endpoint); // api에서 장소 데이터 가져오기
+        setPlaces(data); // 화면에 보여줄 리스트 저장
+        setOriginalPlaces(data); //원본 리스트도 따로 저장
       } catch {
-        console.log('error');
+        setError('에러다');
       } finally {
         setLoading(false);
       }
     };
-    initPlace();
+    initPlace(); // 비동기 함수 호출
   }, [endpoint]);
-
-  useEffect(() => {
-    console.log('places:', places);
-  }, [places]);
 
   const handleClickButton = () => {
     if (locError) return locError;
-    setIsDefaultSort(prev => !prev);
+    //정렬 상태를 반대로 전환하면서 로직 실행
+    setIsDefaultSort(prev => {
+      const next = !prev;
+      if (next) {
+        //거리순 정렬 수행
+        const sorted = sortPlacesByDistance(
+          places,
+          location.latitude,
+          location.longitude,
+        );
+        setPlaces(sorted); // 정렬된 리스트로 세팅
+      } else {
+        //원래대로 복원
+        setPlaces(originalPlaces);
+      }
+      return next;
+    });
   };
 
   return (
